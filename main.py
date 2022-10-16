636c518f61b4846b830b4e940d02a1a18067b63e
Rui Yan <angruiyan@gmail.com>
2022-10-16 23:15:46 +0800

    Removed hash storage. Added merkleTree comparisor. Added CL argument

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from merkleTree import MerkleTree
+from merkleTree import MerkleTree, getDifferenceInTrees
 
 # Hashing content from system1
 def testCase1() -> None:
@@ -17,23 +17,52 @@ def testCase2() -> None:
     root_hash1 = mTree1.getRootHash()
     print("system1:", root_hash1)
 
-    print("")
     mTree2 = MerkleTree("data/system2")
     root_hash2 = mTree2.getRootHash()
     print("system2:", root_hash2)
-    assert root_hash1 == root_hash2, "Different hash values when it should be the same"
+
+    print("There is no difference." \
+        if root_hash1 == root_hash2 else "Different hash values when it should be the same")
     print("")
 
+# Hashing content from system1
+# Hashing content from system2
+# Compare the hashes manually for now
 def testCase3() -> None:
     print("Test case 3:")
     mTree1 = MerkleTree("data/system1")
-    mTree3 = MerkleTree("data/system3")
+    root_hash1 = mTree1.getRootHash()
+    print("system1:", root_hash1)
 
-    difference = MerkleTree.getDifferenceInTrees(mTree1, mTree3)
-    print(difference)
+    mTree3 = MerkleTree("data/system3")
+    root_hash3 = mTree3.getRootHash()
+    print("system3:", root_hash3)
+
+    print("There is no difference." \
+        if root_hash1 == root_hash3 else "Different hash values")
+    print("")
+
+
+# Compare system1 and system2
+# The result should be the same
+def testCase4() -> None:
+    print("Test case 4:")
+    difference = getDifferenceInTrees("data/system1", "data/system2")
+    print("There is no difference." if difference is None else difference)
+    print("")
+
+# Compare system1 and system3
+# The result should be the different
+# It will return list of list of different filename
+def testCase5() -> None:
+    print("Test case 5:")
+    difference = getDifferenceInTrees("data/system1", "data/system3")
+    print("There is no difference." if difference is None else difference)
     print("")
 
 if __name__ == "__main__":
     testCase1()
     testCase2()
     testCase3()
+    testCase4()
+    testCase5()
--- merkleTree.py
+++ merkleTree.py
@@ -1,10 +1,11 @@
 # Inspired by https://trebaud.github.io/posts/merkle-tree/
 
+import sys
 from typing import List
 import hashlib
 from os import walk
 from os.path import join
-import configparser, json
+import argparse
 
 # Create a node class
 class Node:
@@ -12,7 +13,7 @@ class Node:
         self.left: Node = left
         self.right: Node = right
         self.value = value
-        self.fileName = fileName if fileName is not None else ""
+        self.fileName = fileName if fileName is not None else None
 
     def __str__(self) -> str:
         return str(self.value)
@@ -26,14 +27,6 @@ class MerkleTree:
     def __init__(self, filePath: str, hashStoragePath: str = None) -> None:
         values = self._getAllFileContent(filePath)
         self._buildTree(values)
-        if hashStoragePath is not None:
-            self.hashStoragePath = hashStoragePath 
-        else:
-            config = configparser.ConfigParser()
-            config.read('config.sys')
-            self.hashStoragePath = config.get("System","HashStorage")
-
-        self._saveRootHash(self.root.value, filePath, self.hashStoragePath)
 
     # Gets all the content from the files in the path
     def _getAllFileContent(self, filePath: str):
@@ -98,20 +91,6 @@ class MerkleTree:
 
     def getRootHash(self)-> str:
         return self.root.value
-        
-    # Store the hashes somewhere
-    def _saveRootHash(self, hash, systemName, filePath):
-        # Read the data, return empty if null
-        with open(filePath, 'r') as f:
-            try:
-                data = json.load(f)
-            except json.JSONDecodeError:
-                data = {}
-        
-        # update the hashes
-        with open(filePath, 'w') as f:
-            data[systemName] = hash
-            json.dump(data, f, indent=4)
 
     # accessible without declaring
     # SHA256
@@ -119,16 +98,52 @@ class MerkleTree:
     def hash(value: str) -> str:
         return hashlib.sha256(value.encode('utf-8')).hexdigest()
 
-    # Static method that compares two file system
-    # It determines 
-    # If they are the same, NOTHING happens
-    # Else, they search down the hashes (stored in somewhere) to find the difference
-    @staticmethod
-    def getDifferenceInTrees(merkleTree1, merkleTree2):
-        if merkleTree1.root.value == merkleTree2.root.value:
-            return None
-        else:
-            # Loop through to find the difference
-            return True
+# Compares two file system
+# It determines 
+# If they are the same, NOTHING happens
+# Else, they search down the hashes (stored in somewhere) to find the difference
+def getDifferenceInTrees(merkleTree1_filePath: str, merkleTree2_filePath: str):
 
-    # Searches for nodes
+    difference = []
+    merkleTree1 = MerkleTree(merkleTree1_filePath)
+    merkleTree2 = MerkleTree(merkleTree2_filePath)
+    
+    def _comparisonHelper(node1: Node, node2: Node):
+        if node1 is not None or node2 is not None:
+            _comparisonHelper(node1.left, node2.left)
+            if node1.value != node2.value and (node1.fileName is not None and node2.fileName is not None):
+                # Store in a list of differences
+                difference.append([node1.fileName, node2.fileName])
+            _comparisonHelper(node1.right, node2.right)
+
+    if merkleTree1.root is None or merkleTree2.root is None:
+        return None
+
+    if merkleTree1.root.value == merkleTree2.root.value:
+        return None
+    else:
+        # Find the difference
+        _comparisonHelper(merkleTree1.root, merkleTree2.root)
+        return difference
+
+
+if __name__ == '__main__':  
+    parser = argparse.ArgumentParser(description='Welcome to Merkle Tree!')
+    parser.add_argument("--f1", type=str, help="Enter a folder path and get the root hash")
+    parser.add_argument("--f2", type=str, help="[Optional] Enter a second folder path and compare the root hash")
+    
+    args = parser.parse_args()
+
+    # When there is only the second argument
+    if args.f1 is None and args.f2 is not None:
+        parser.error('--f1 and --f2 must be given together or use only --f1')
+    # When there are both
+    elif args.f1 is not None and args.f2 is not None: 
+        difference = getDifferenceInTrees(args.f1, args.f2)
+        print("There is no difference." if difference is None else difference)
+    # When there is only the first argument
+    elif args.f1 is not None:
+        mTree1 = MerkleTree(args.f1)
+        print(mTree1.getRootHash())
+
+    
